3be52799dd2f8946407d1f29e2e585e720a3df1e
Victor Mateus <victormateus001@hotmail.com>
2021-04-08 11:45:53 -0300

    👾 | Agora ele transfoma gifs em stickers

--- scr/commands/sticker.js
+++ scr/commands/sticker.js
@@ -1,12 +1,6 @@
-/*
-
-    MANO EU IMPLORO, ALGUÉM ME ARRUMA UM CONVERSOR DE MP4 PARA GIF SEM SER O FFMPEG PLS PLS PLS, ESSA MERDA DE WHATSAPP RECEBE OS ARQUIVOS GIF COMO MP4,
-    ASSIM NÃO TEM COMO ENVIAR UM STICKER ANIMADO SEM ANTES CONVERTE-LO!!!
-
-*/
 const Commands = require("../config/commands");
+const ffmpeg = require('fluent-ffmpeg-extended');
 const mime = require('mime-types');
-
 const mkdirp = require('mkdirp');
 const { writeFile, unlinkSync } = require('fs');
 
@@ -14,7 +8,7 @@ module.exports = class StickerCommand extends Commands {
     constructor(zap) {
         super(zap, {
             name: 'sticker',
-            aliases: ['figurinha'],
+            aliases: ['s', 'figurinha'],
             category: 'utils',
             ownerOnly: false
         })
@@ -22,11 +16,19 @@ module.exports = class StickerCommand extends Commands {
     async execute({ client, msg }) {
         const buffer = await client.decryptFile(msg);
         const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const archive = `${randomString}.${mime.extension(msg.mimetype)}`
-        const path = `.temp/${archive}`;
+        const archivename = randomString
+        let path = `.temp/${archivename}.${mime.extension(msg.mimetype)}`;
 
         await mkdirp(".temp/");
         writeFile(path, buffer, async () => {
+            if (mime.extension(msg.mimetype) === "mp4") {
+                return ffmpeg({ source: path }).saveToFile(`.temp/${archivename}.gif`, async () => {
+                    unlinkSync(path)
+                    path = `.temp/${archivename}.gif`
+                    await client.sendImageAsStickerGif(msg.from, path)
+                    unlinkSync(path)
+                });
+            }
             await client.sendImageAsSticker(msg.from, path)
             unlinkSync(path)
         });
