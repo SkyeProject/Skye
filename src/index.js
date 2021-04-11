d9427c4499f7f78c0bfb986f9519f4b2493a795f
Victor Mateus <victormateus001@hotmail.com>
2021-04-10 22:29:07 -0300

    Corrigindo localizações depois da mudança

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const AtizapClient = require("./config/AtizapClient");
 const config = require("../config.json")
 const zap = new AtizapClient()
 
-zap.loadEvents("./scr/events/**/*.js")
-zap.loadCommands("./scr/commands/**/*.js")
+zap.loadEvents("./src/events/**/*.js")
+zap.loadCommands("./src/commands/**/*.js")
 
 module.exports = {
     zap,
