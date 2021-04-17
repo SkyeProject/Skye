4df44f48b0b010364a2a20794dbd4bb77ce99e31
matheustorsani <zkingdragoon@gmail.com>
2021-04-17 16:26:15 -0300

    Comando Rainbow pronto!

--- src/commands/canvas.js
+++ src/commands/canvas.js
@@ -1,27 +1,24 @@
 const Commands = require('../config/commands')
 const Canvacord = require('canvacord')
 module.exports = class CanvasCommand extends Commands {
-  constructor (zap) {
+  constructor(zap) {
     super(zap, {
-      name: 'trige',
-      aliases: ['t'],
-      category: 'utils',
-      ownerOnly: true
+      name: 'rainbow',
+      aliases: ['lgbt', 'gay'],
+      category: 'fun',
+      ownerOnly: false
     })
   }
 
-  async execute ({ msg }) {
-    //const shipOne = await msg.getContact(args[0].replace('@', '') + '@c.us') ISSO É PRA PEGAR A FOTO DO USUARIO MENCIONADO 
-
-    if(!msg.isGroupMsg){
-    const user = await msg.getContact(msg.from, msg.getSenderNumber())
-    const teste = await Canvacord.Canvas.rainbow(user.avatar);
-    msg.sendImage(`data:image/png;base64,${teste.toString('base64')}`)
-    } else{
-      const user1 = await msg.sender.profilePicThumbObj.eurl
-      console.log(msg.sender.profilePicThumbObj.eurl)
-      const teste1 = await Canvacord.Canvas.rainbow(user1);
-    msg.sendImage(`data:image/png;base64,${teste1.toString('base64')}`)
+  async execute({ msg, args }) {
+    if (!args[0]) {
+      const user = await msg.sender.profilePicThumbObj.eurl
+      const rainbow = await Canvacord.Canvas.rainbow(user);
+      msg.sendImage(`data:image/png;base64,${rainbow.toString('base64')}`)
+    } else {
+      const userMention = await msg.getContact(args[0].replace('@', '') + '@c.us')
+      const rainbow = await Canvacord.Canvas.rainbow(userMention.avatar);
+      msg.sendImage(`data:image/png;base64,${rainbow.toString('base64')}`)
     }
-  } 
+  }
 }
--- src/commands/help.js
+++ src/commands/help.js
@@ -11,6 +11,6 @@ module.exports = class HelpCommand extends Commands {
   }
 
   execute ({ msg, prefix }) {
-    msg.send(`SCHWAP COMMANDS\n\n${prefix}help, ajuda: Lista dos meus comandos\n\n${prefix}Oi: Irei te responder (?)\n\n${prefix}sticker, figurinha, s: Cria uma figurinha com o conteudo desejado.\nEx.: ${prefix}sticker Hello World! ou !sticker (foto)\n\n${prefix}neko, kwaii, gata: Busca uma neko pra você :3\n\n${prefix}ping, ms: Calcula a latência\n\n${prefix}everyone, mentionall, all, mention: Menciona todos os membros de um grupo (use !everyone *(mensagem para marcar com uma mensagem)*\n\n${prefix}calcular, math, c: Calcula dois valores que voce desejar\n\n${prefix}Ship, amor: Calcula o amor de duas pessoas!\n\n${prefix}amongus, among, impostor: Quem sera o impostor?\n\n\nSchwap 😎`)
+    msg.send(`SCHWAP COMMANDS\n\n${prefix}help, ajuda: Lista dos meus comandos\n\n${prefix}Oi: Irei te responder (?)\n\n${prefix}sticker, figurinha, s: Cria uma figurinha com o conteudo desejado.\nEx.: ${prefix}sticker Hello World! ou !sticker (foto)\n\n${prefix}neko, kwaii, gata: Busca uma neko pra você :3\n\n${prefix}ping, ms: Calcula a latência\n\n${prefix}everyone, mentionall, all, mention: Menciona todos os membros de um grupo (use !everyone *(mensagem para marcar com uma mensagem)*\n\n${prefix}calcular, math, c: Calcula dois valores que voce desejar\n\n${prefix}Ship, amor: Calcula o amor de duas pessoas!\n\n${prefix}amongus, among, impostor: Quem sera o impostor?\n\n/rainbow, lgbt, gay: Que tal dar um toque Arco-Iris na sua foto?\n\n\nSchwap 😎`)
   }
 }
