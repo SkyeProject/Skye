d0ae9bff6119af13160cdb2ebf154cdc157789ed
Demetrius Matheus <68620332+matheustorsani@users.noreply.github.com>
2021-04-17 18:38:57 -0300

    Atualiazando Rainbow

--- src/commands/rainbow.js
+++ src/commands/rainbow.js
@@ -22,7 +22,7 @@ module.exports = class CanvasCommand extends Commands {
         msg.sendImage(`data:image/png;base64,${rainbow.toString('base64')}`)
       }
     } catch (error) {
-      msg.send(`Algo deu errado, talvez você não tem foto ou ela esta privada. Se não é nenhum dos dois, peça para alguem fazer pra você, se o erro ainda insistir, contate o +5511951746304, Infelizmente a API do whatsapp é um lixo e as vezes recusa de mim pegar a foto de alguns... :(\n\n\n${error}`)
+      msg.send(`Algo deu errado, talvez você não tem foto ou ela esta privada. Se não é nenhum dos dois, peça para alguem fazer pra você, se o erro ainda insistir, contate o +5511951746304, Infelizmente a API do whatsapp é um lixo e as vezes me impede de pegar a foto de alguns... :(\n\n\n${error}`)
     }
   }
 }
