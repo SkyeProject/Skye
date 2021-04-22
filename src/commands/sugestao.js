c2e1aeda597ebe6a587ed36973d20da54343d0fb
Demetrius Matheus <68620332+matheustorsani@users.noreply.github.com>
2021-04-22 20:51:08 -0300

    Comando de sugestão adicionado!

---
+++ src/commands/sugestao.js
@@ -0,0 +1,23 @@
+const Commands = require('../config/commands')
+
+module.exports = class SugestaoCommand extends Commands {
+    constructor(zap) {
+        super(zap, {
+            name: 'sugestao',
+            aliases: ['sug', 'ideia', 'sugestão'],
+            category: 'utils',
+            ownerOnly: false
+        })
+    }
+
+    async execute({ msg, args }) {
+        let message = args.join(' ')
+        let name = msg.sender.pushname;
+        if (!message) {
+            msg.send('Você enviou uma sugestão, espera, acho que não... ')
+        } else {
+            msg.send('Sugestão enviada!')
+            await this.zap.atizap.sendText('5511953532681-1619128440@g.us', `Opa! Sugestão nova: *${message}*\n\nSugestão enviada de: ${name}`);
+        }
+    }
+}
