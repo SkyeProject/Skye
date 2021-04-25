c01aa9274a2ff60ee5516c2e637ea526a42a39ab
matheustorsani <zkingdragoon@gmail.com>
2021-04-25 17:05:02 -0300

    Consertando hug

--- src/commands/fun/hug.js
+++ src/commands/fun/hug.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 const superagent = require('superagent')
 const Commands = require('../../config/commands')
 
@@ -20,16 +18,16 @@ module.exports = class HugCommand extends Commands {
 
   async execute ({ msg, args }) {
     try {
-        const nekoimage = (await superagent.get('https://nekos.life/api/v2/img/hug')).body
-        let me = msg.sender.pushname
-        if(!args[0]){
-            msg.send(`Abraçei bem forte o ${me}! ❤️`)
-            return this.zap.atizap.sendImageAsSticker(msg.from, nekoimage.url, null)
-        }else {
-        let args0 = await msg.getContact(args[0].replace('@', ''))
+      const nekoimage = (await superagent.get('https://nekos.life/api/v2/img/hug')).body
+      const me = msg.sender.pushname
+      if (!args[0]) {
+        msg.send(`Abraçei bem forte o ${me}! ❤️`)
+        return this.zap.atizap.sendImageAsSticker(msg.from, nekoimage.url, null)
+      } else {
+        const args0 = await msg.getContact(args[0].replace('@', ''))
         msg.send(`${me} deu aquele abraço em ${args0.username}`)
         this.zap.atizap.sendImageAsSticker(msg.from, nekoimage.url, null)
-    }
+      }
     } catch (err) {
       msg.zapFail(err)
     }
