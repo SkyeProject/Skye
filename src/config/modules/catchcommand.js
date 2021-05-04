9a3fd7ed57ffd737ddb0a740f155787e314526a9
Victor Mateus <victormateus001@hotmail.com>
2021-05-04 15:57:03 -0300

    tentando resolver problema com rate limite das fotos, e agora temos cmd do akinator 😎😎

--- src/config/modules/catchcommand.js
+++ src/config/modules/catchcommand.js
@@ -1,12 +1,11 @@
 const { zap, config } = require('../../index')
 const DiscordEmbed = require('./DiscordEmbedHook')
-module.exports = async (user, msg) => {
+module.exports = async (msg) => {
   const groupName = msg.chat.groupMetadata ? msg.chat.name : 'O usuário mandou no privado.'
   const groupPic = msg.chat.groupMetadata ? await zap.atizap.getProfilePicFromServer(msg.from) : ''
-
   new DiscordEmbed()
     .setWebhook(config.discord.webhookUrlforCommands)
-    .setAuthor(`${user.username} | ${user.number.replace('@c.us', '')}`, user.avatar)
+    .setAuthor(`${msg.sender.verifiedName || msg.sender.shortName || msg.sender.formattedName || msg.sender.name} | ${msg.sender.id.replace('@c.us', '')}`, msg.sender.profilePicThumbObj.eurl)
     .setTitle('Usou o comando:')
     .setDescription(msg.content)
     .setColor('#94bae0')
