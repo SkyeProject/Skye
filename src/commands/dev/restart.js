e586a09efc4b2a2deccc8e60f359b39358ca5a8c
Victor Mateus <victormateus001@hotmail.com>
2021-05-05 13:25:26 -0300

    ok, agora o bot vai reiniciar a cada 1 hora

---
+++ src/commands/dev/restart.js
@@ -0,0 +1,32 @@
+const Command = require('../../config/Command')
+const pm2 = require('pm2')
+const sleep = require('sleep-promise')
+
+module.exports = class RestartCommand extends Command {
+  constructor (zap) {
+    super(zap, {
+      name: 'restart',
+      aliases: ['reiniciarbot'],
+      category: 'dev',
+      description: 'Reinicie o bot.',
+      example: 'restart',
+      onlyGroup: false,
+      groupAdmPermission: {
+        bot: false,
+        user: false
+      },
+      ownerOnly: true
+    })
+  }
+
+  async execute ({ msg }) {
+    try {
+      await msg.send('Beleza, reiniciando bot...')
+      global.restart = true
+      await sleep(10000)
+      pm2.restart('.')
+    } catch (err) {
+      await msg.zapFail(err)
+    }
+  }
+}
