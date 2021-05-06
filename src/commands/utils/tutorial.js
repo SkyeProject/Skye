df87ddf96536e310e595cf7f2a99bc2e2d116406
matheustorsani <zkingdragoon@gmail.com>
2021-05-06 15:22:36 -0300

    Tutorial atualizado 2021 Como mexer no bot

---
+++ src/commands/utils/tutorial.js
@@ -0,0 +1,27 @@
+const Command = require('../../config/Command')
+
+module.exports = class TutorialCommand extends Command {
+  constructor (zap) {
+    super(zap, {
+      name: 'Tutorial',
+      aliases: ['skye', 'como', 'comofunciona', 'comousar'],
+      description: 'Tutorial de como usar a skye.',
+      example: 'tutorial',
+      category: 'utils',
+      groupOnly: false,
+      groupAdmPermission: {
+        bot: false,
+        user: false
+      },
+      ownerOnly: false
+    })
+  }
+
+  async execute ({ msg, prefix }) {
+    try {
+      await msg.send(`Tutorial de como me usar: https://youtu.be/aAH4kGd09AI \nDuvidas use ${prefix}grupo`, { youtube: true })
+    } catch (err) {
+      await msg.zapFail(err)
+    }
+  }
+}
